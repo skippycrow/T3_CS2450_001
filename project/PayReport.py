47c7f1c70f7803a0eb5883ff6d8f5c60bc099870
Jaedon-UVU <10833253@my.uvu.edu>
2021-10-11 12:27:33 -0600

    Added commented code for: calculate_deductions(search) calculate_overtime_pay(search) calculate_pay(search)
    Current issues
    #Unable to retrive permission level from Employee class
    #Unable to retrieve rate from Employee class
    #Unable to retrieve horus from Employee class

--- project/PayReport.py
+++ project/PayReport.py
@@ -1,7 +1,7 @@
 from database import Employee
 from database import EmployeeDatabase
 
-def pay_report_frame(user, search):
+def pay_report(user, search):
 	"""
 	Description
 	-----------
@@ -16,12 +16,13 @@ def pay_report_frame(user, search):
 	"""
 	# Check user information and permissions
 	if(CheckPermissions()):
+		#TODO: Show searched employee payreport
 		pass
-	pass
+	else:
+		#TODO: Popup window that informs the user that they do not have the permissiosn
+		pass
 
 #FIXME: Unable to retrieve employee permission level
-
-
 def allow_access(user, search):
 	"""
 	Description
@@ -44,4 +45,66 @@ def allow_access(user, search):
 	if user.get_id() == search.get_id(): # or user.role == admin
 		return True
 	return False
+
+#TODO: Define calculate_deductions(search)
+def calculate_deductions(search):
+	"""
+	Description
+	-----------
+	Calculate the deductions to the employee's pay
+
+	Parameters
+	----------
+	search : Employee
+		The employee who's information is being accessed
 	
+	Returns
+	-------
+	decimal
+		Deduction ammount
+	"""
+	pass
+
+#FIXME: Unable to retrieve employee rate and hours
+def calculate_overtime_pay(search):
+	"""
+	Description
+	-----------
+	Calculate the employee pay for overtime
+
+	Parameters
+	----------
+	search : Employee
+		The employee who's information is being accessed
+	
+	Returns
+	-------
+	decimal
+		Overtime pay ammount
+	"""
+	#hours = search.hours - 40 if search.hours > 40 else 0
+	#rate = search.rate * 1.5
+	#return hours * rate
+	pass
+
+#FIXME: Unable to retrieve employee rate and hours
+def calculate_pay(search):
+	"""
+	Description
+	-----------
+	Calculate the employee pay
+
+	Parameters
+	----------
+	search : Employee
+		The employee who's information is being accessed
+	
+	Returns
+	-------
+	decimal
+		Pay ammount
+	"""
+	#hours = 40 if search.hours > 40 else search.hours
+	#rate = search.rate
+	#return hours * rate
+	pass
