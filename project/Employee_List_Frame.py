9e43a1960831c305d7231623ab88b15518f823a8
ACast1 <91992975+ACast1@users.noreply.github.com>
2021-10-21 01:50:32 -0600

    Made class compatible with abstracted controller

--- project/Employee_List_Frame.py
+++ project/Employee_List_Frame.py
@@ -1,39 +1,5 @@
 import tkinter as tk
-
-#testing
-
-class EmployeeApp(tk.Tk):
-    def __init__(self):
-        #Initialize tk
-        tk.Tk.__init__(self)
-        
-        #Initialize container options
-        container = tk.Frame(self)
-        container.pack(side = "top", fill = "both", expand = True)
-        container.grid_rowconfigure(0, weight = 1)
-        container.grid_columnconfigure(0, weight = 1)
-
-        #Initialize list to hold frames
-        self.frames = {}
-        
-
-        #Initialize frame values
-        page_name = EmployeeList.__name__
-        frame = EmployeeList(parent = container, controller = self)
-        self.frames[page_name] = frame
-
-            #Stack each frame in same grid cell
-        frame.grid(row = 0, column = 0, sticky = "nesw")
-
-        #Show the login page
-        self.present_frame("EmployeeList")
-
-    def present_frame(self, page_name):
-        #Set the frame
-        frame = self.frames[page_name]
-        #Raise the frame to top of list
-        frame.tkraise()
-
+from tkinter import messagebox as msg
 
 #TODO
 #Search results function
@@ -43,40 +9,39 @@ class EmployeeApp(tk.Tk):
 #Search button doesn't utilize the database
 #Clicking on the employee doesn't select employee.
 
-
 class EmployeeList(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        
         #self.employees_database = employees_database
         tk.Label(self, text = "Search Employee ", fg = "black", font = "none 12 bold").grid(row = 1, column = 0, sticky = tk.W)
-        self.searchBox = tk.Entry(self, width = 73, bg = "white").grid(row=2, column =0, sticky = tk.NW)
-        self.searchButton = tk.Button(self, text = "Search", width = 20, bg = "white", command = self.searchEmployee)
-        self.searchButton.grid(row= 2, column = 0, sticky = tk.NE)
-        #
-        self.search_Results = ["John Morgan        #91423", "Spencer Crow       #83523"]
-        self.resultBox = tk.Listbox(self, width = 100, bg = "white")
-        self.resultBox.grid(row=3, column =0, sticky = tk.NW)
-        self.selectedEmployee = None
+        self.search_box = tk.Entry(self, width = 73, bg = "white").grid(row=2, column =0, sticky = tk.NW)
+        self.search_button = tk.Button(self, text = "Search", width = 20, bg = "white", command = self.search_employee)
+        self.search_button.grid(row= 2, column = 0, sticky = tk.NE)
+        
+        self.search_results = ["John Morgan        #91423", "Spencer Crow       #83523"]
+        self.result_box = tk.Listbox(self, width = 100, bg = "white")
+        self.result_box.grid(row=3, column =0, sticky = tk.NW)
+        self.selected_employee = None
         
         #populate the search Results
-        for item in self.search_Results:
-            self.resultBox.insert(tk.END, item)
+        for item in self.search_results:
+            self.result_box.insert(tk.END, item)
         #event handler for when clicked. selectEmployee()
-        self.resultBox.bind("<<ListboxSelect>>", self.selectEmployee)
+        self.result_box.bind("<<ListboxSelect>>", self.select_employee)
         
-        self.addEmployeeButton = tk.Button(self, text = "Add Employee", width = 20, bg = "white", command = lambda: controller.preset_frame("NewEmployeeFrame"))
-        self.addEmployeeButton.grid(row=4, column= 0, sticky = tk.NW)
+        self.add_employee_button = tk.Button(self, text = "Add Employee", width = 20, bg = "white", command = lambda: controller.present_frame("AddEmployee"))
+        self.add_employee_button.grid(row=4, column= 0, sticky = tk.NW)
         
-        self.editEmployeeButton = tk.Button(self, text = "Edit Employee", width = 20, bg = "white", command = lambda: controller.preset_frame("EditEmployeeFrame"))
-        self.editEmployeeButton.grid(row=5, column= 0, sticky = tk.NW)
+        self.edit_employee_button = tk.Button(self, text = "Edit Employee", width = 20, bg = "white", command = lambda: controller.present_frame("EditEmployee"))
+        self.edit_employee_button.grid(row=5, column= 0, sticky = tk.NW)
 
-        self.payrollButton = tk.Button(self, text = "Payroll", width = 20, bg = "white", command = lambda: controller.preset_frame("payrollFrame"))
-        self.payrollButton.grid(row=4, column= 0, sticky = tk.E)
+        self.payroll_button = tk.Button(self, text = "Payroll", width = 20, bg = "white", command = lambda: controller.preset_frame("payrollFrame"))
+        self.payroll_button.grid(row=4, column= 0, sticky = tk.E)
 
-        self.exportEmployeeButton = tk.Button(self, text = "Export Employee", width = 20, bg = "white", command = lambda: controller.preset_frame("exportEmployee"))
-        self.exportEmployeeButton.grid(row=5, column= 0, sticky = tk.E)
-
+        self.export_employee_button = tk.Button(self, text = "Export Employee", width = 20, bg = "white", command = lambda: controller.present_frame("exportEmployee"))
+        self.export_employee_button.grid(row=5, column= 0, sticky = tk.E)
         
         #copys the selected employee to selectedEmployee
     def selectEmployee(self, event):
@@ -85,15 +50,3 @@ class EmployeeList(tk.Frame):
         
     def searchEmployee(self):
         pass
-                                           
-    
-    
-    
-
-def main():
-
-    app = EmployeeApp()
-    app.mainloop()
-
-if __name__ == "__main__":
-    main()
