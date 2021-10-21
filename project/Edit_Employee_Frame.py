e449c33208faca878eed32645a8b9d297f9ac5b3
ACast1 <91992975+ACast1@users.noreply.github.com>
2021-10-21 01:53:36 -0600

    Made class compatible with abstracted controller

--- project/Edit_Employee_Frame.py
+++ project/Edit_Employee_Frame.py
@@ -1,65 +1,49 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from tkinter import messagebox as msg
 
+class EditEmployee(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-class EditEmployee:
-    def __init__(self):
-        root = Tk()
-        root.title("Edit Employee")
-        frm = ttk.Frame(root, padding=15)
-        frm.grid()
-        cancel = ttk.Style()
-        cancel.configure('C.TButton', background='white', foreground='black', font=('Arial', 14))
+        self.controller = controller
 
-        submit = ttk.Style()
-        submit.configure('S.TButton', background='white', foreground='black', font=('Arial', 14))
+        title_label = tk.Label(self, text="Edit Employee - Will be populated with employee info").grid(row=0, columnspan=2, pady=10, sticky=tk.N)
 
-        title_label = Label(root, text="Edit Employee - Will be populated with employee info").grid(row=0, columnspan=2, pady=10, sticky="N")
-
-        edit_first_name_label = Label(root, text="First Name:").grid(row=1, column=0, pady=5, sticky="W")
-        edit_first_name_entry = Entry().grid(row=1, column=1)
-        edit_last_name_label = Label(root, text="Last Name:").grid(row=2, column=0, pady=5, sticky="W")
-        edit_last_name_entry = Entry().grid(row=2, column=1)
-        edit_street_label = Label(root, text="Street:").grid(row=3, column=0, pady=5, sticky="W")
-        edit_street_entry = Entry().grid(row=3, column=1)
-        edit_city_label = Label(root, text="City:").grid(row=4, column=0, pady=5, sticky="W")
-        edit_city_entry = Entry().grid(row=4, column=1)
-        edit_state_label = Label(root, text="State:").grid(row=5, column=0, pady=5, sticky="W")
-        edit_state_entry = Entry().grid(row=5, column=1)
-        edit_zip_label = Label(root, text="Zip:").grid(row=6, column=0, pady=5, sticky="W")
-        edit_zip_entry = Entry().grid(row=6, column=1)
-        edit_phone_label = Label(root, text="Phone:").grid(row=7, column=0, pady=5, sticky="W")
-        edit_phone_entry = Entry().grid(row=7, column=1)
-        edit_routing_number_label = Label(root, text="Routing Number:").grid(row=8, column=0, pady=5, sticky="W")
-        edit_routing_number_entry = Entry().grid(row=8, column=1)
-        edit_account_number_label = Label(root, text="Accounting Number:").grid(row=9, column=0, pady=5, sticky="W")
-        edit_account_number_entry = Entry().grid(row=9, column=1)
-        edit_payment_method_label = Label(root, text="Pay Method:").grid(row=10, column=0, pady=5, sticky="W")
-        edit_payment_method_entry = Entry().grid(row=10, column=1)
-        submit_button = ttk.Button(text="Submit", style="S.TButton", command = self.save_info).grid(row=11, column=1, pady=15)
-        cancel_button = ttk.Button(text="Cancel", style="C.TButton", command=root.destroy).grid(row=11, column=0)
-
-        root.mainloop()
+        edit_first_name_label = tk.Label(self, text="First Name:").grid(row=1, column=0, pady=5, sticky=tk.W)
+        edit_first_name_entry = tk.Entry(self, width = 15, bg = "white").grid(row=1, column=1)
+        edit_last_name_label = tk.Label(self, text="Last Name:").grid(row=2, column=0, pady=5, sticky=tk.W)
+        edit_last_name_entry = tk.Entry(self, width = 15, bg = "white").grid(row=2, column=1)
+        edit_street_label = tk.Label(self, text="Street:").grid(row=3, column=0, pady=5, sticky=tk.W)
+        edit_street_entry = tk.Entry(self, width = 15, bg = "white").grid(row=3, column=1)
+        edit_city_label = tk.Label(self, text="City:").grid(row=4, column=0, pady=5, sticky=tk.W)
+        edit_city_entry = tk.Entry(self, width = 15, bg = "white").grid(row=4, column=1)
+        edit_state_label = tk.Label(self, text="State:").grid(row=5, column=0, pady=5, sticky=tk.W)
+        edit_state_entry = tk.Entry(self, width = 15, bg = "white").grid(row=5, column=1)
+        edit_zip_label = tk.Label(self, text="Zip:").grid(row=6, column=0, pady=5, sticky=tk.W)
+        edit_zip_entry = tk.Entry(self, width = 15, bg = "white").grid(row=6, column=1)
+        edit_phone_label = tk.Label(self, text="Phone:").grid(row=7, column=0, pady=5, sticky=tk.W)
+        edit_phone_entry = tk.Entry(self, width = 15, bg = "white").grid(row=7, column=1)
+        edit_routing_number_label = tk.Label(self, text="Routing Number:").grid(row=8, column=0, pady=5, sticky=tk.W)
+        edit_routing_number_entry = tk.Entry(self, width = 15, bg = "white").grid(row=8, column=1)
+        edit_account_number_label = tk.Label(self, text="Accounting Number:").grid(row=9, column=0, pady=5, sticky=tk.W)
+        edit_account_number_entry = tk.Entry(self, width = 15, bg = "white").grid(row=9, column=1)
+        edit_payment_method_label = tk.Label(self, text="Pay Method:").grid(row=10, column=0, pady=5, sticky=tk.W)
+        edit_payment_method_entry = tk.Entry(self, width = 15, bg = "white").grid(row=10, column=1)
+        submit_button = tk.Button(self, text="Submit", command = self.save_info).grid(row=11, column=1, pady=15)
+        cancel_button = tk.Button(self, text="Cancel", command= self.save_info).grid(row=11, column=0)
 
     def save_info(self):
         """Save the new Employee in the database using the database functions"""
-        popup = Tk()
-        popup.wm_title("Saved")
-        label = Label(popup, text="Employee data will be saved")
-        label.pack(side="top", fill="x", pady=10)
-        B1 = Button(popup, text="Okay", command = popup.destroy)
-        B1.pack()
-        popup.mainloop()
+        msg.showinfo(title="Saved", message="Employee data will be saved")
         #todo
-        #Save data to database
-        #Return to previous frame
 
+        #Save data to database
+
+        #Return to previous frame
+        self.controller.present_frame("EmployeeList")
 
     def check_permission(self, id):
         """returns true if logged in employee matches profile id#
         returns true if logged in as administrator
         returns false otherwise"""
         return False
-
-
-EditEmployee()
