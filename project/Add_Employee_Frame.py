e1191bc5f1d9ec60522542030c10f36ba97ea217
skippycrow <70984355+skippycrow@users.noreply.github.com>
2021-10-28 17:33:53 -0600

    Additional Database Fields added

--- project/Add_Employee_Frame.py
+++ project/Add_Employee_Frame.py
@@ -1,48 +1,141 @@
 import tkinter as tk
 from tkinter import messagebox as msg
 
+
 class AddEmployee(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
         self.controller = controller
+        self.pay_method = tk.StringVar(self)
 
-        title_label = tk.Label(self, text="Add Employee").grid(row=0, columnspan=2, pady=10, sticky= tk.N)
+        self.title_label = tk.Label(self, text="Add Employee")
+        self.title_label.grid(row=0, columnspan=6, pady=10, sticky=tk.N)
 
-        edit_first_name_label = tk.Label(self, text="First Name:").grid(row=1, column=0, pady=5, sticky= tk.W)
-        edit_first_name_entry = tk.Entry(self, width = 15, bg = "white").grid(row=1, column=1)
-        edit_last_name_label = tk.Label(self, text="Last Name:").grid(row=2, column=0, pady=5, sticky=tk.W)
-        edit_last_name_entry = tk.Entry(self, width = 15, bg = "white").grid(row=2, column=1)
-        edit_street_label = tk.Label(self, text="Street:").grid(row=3, column=0, pady=5, sticky=tk.W)
-        edit_street_entry = tk.Entry(self, width = 15, bg = "white").grid(row=3, column=1)
-        edit_city_label = tk.Label(self, text="City:").grid(row=4, column=0, pady=5, sticky=tk.W)
-        edit_city_entry = tk.Entry(self, width = 15, bg = "white").grid(row=4, column=1)
-        edit_state_label = tk.Label(self, text="State:").grid(row=5, column=0, pady=5, sticky=tk.W)
-        edit_state_entry = tk.Entry(self, width = 15, bg = "white").grid(row=5, column=1)
-        edit_zip_label = tk.Label(self, text="Zip:").grid(row=6, column=0, pady=5, sticky=tk.W)
-        edit_zip_entry = tk.Entry(self, width = 15, bg = "white").grid(row=6, column=1)
-        edit_phone_label = tk.Label(self, text="Phone:").grid(row=7, column=0, pady=5, sticky=tk.W)
-        edit_phone_entry = tk.Entry(self, width = 15, bg = "white").grid(row=7, column=1)
-        edit_routing_number_label = tk.Label(self, text="Routing Number:").grid(row=8, column=0, pady=5, sticky=tk.W)
-        edit_routing_number_entry = tk.Entry(self, width = 15, bg = "white").grid(row=8, column=1)
-        edit_account_number_label = tk.Label(self, text="Accounting Number:").grid(row=9, column=0, pady=5, sticky=tk.W)
-        edit_account_number_entry = tk.Entry(self, width = 15, bg = "white").grid(row=9, column=1)
-        edit_payment_method_label = tk.Label(self, text="Pay Method:").grid(row=10, column=0, pady=5, sticky=tk.W)
-        edit_payment_method_entry = tk.Entry(self, width = 15, bg = "white").grid(row=10, column=1)
-        submit_button = tk.Button(self, text="Submit", command = self.save_info).grid(row=11, column=1, pady=15)
-        cancel_button = tk.Button(self, text="Cancel", command = self.save_info).grid(row=11, column=0)
+        # Emp Id
+        self.add_emp_id_label = tk.Label(self, text="ID:")
+        self.add_emp_id_label.grid(row=1, column=0, pady=5, sticky=tk.W)
+        self.add_emp_id_entry = tk.Entry(self, width=15, bg="white")
+        self.add_emp_id_entry.grid(row=1, column=1)
+
+        # First Name
+        self.add_first_name_label = tk.Label(self, text="First Name:")
+        self.add_first_name_label.grid(row=1, column=2, pady=5, sticky=tk.W)
+        self.add_first_name_entry = tk.Entry(self, width=15, bg="white")
+        self.add_first_name_entry.grid(row=1, column=3)
+
+        # Last Name
+        self.add_last_name_label = tk.Label(self, text="Last Name:")
+        self.add_last_name_label.grid(row=1, column=4, pady=5, sticky=tk.W)
+        self.add_last_name_entry = tk.Entry(self, width=15, bg="white")
+        self.add_last_name_entry.grid(row=1, column=5)
+
+        # Street
+        self.add_street_label = tk.Label(self, text="Street:")
+        self.add_street_label.grid(row=2, column=0, pady=5, sticky=tk.W)
+        self.add_street_entry = tk.Entry(self, width=15, bg="white")
+        self.add_street_entry.grid(row=2, column=1)
+
+        # City
+        self.add_city_label = tk.Label(self, text="City:").grid(row=2, column=2, pady=5, sticky=tk.W)
+        self.add_city_entry = tk.Entry(self, width=15, bg="white").grid(row=2, column=3)
+
+        # State
+        self.add_state_label = tk.Label(self, text="State:").grid(row=2, column=4, pady=5, sticky=tk.W)
+        self.add_state_entry = tk.Entry(self, width=15, bg="white").grid(row=2, column=5)
+
+        # Zip
+        self.add_zip_label = tk.Label(self, text="Zip:").grid(row=3, column=0, pady=5, sticky=tk.W)
+        self.add_zip_entry = tk.Entry(self, width=15, bg="white").grid(row=3, column=1)
+
+        # Phone
+        self.add_phone_label = tk.Label(self, text="Phone:").grid(row=3, column=2, pady=5, sticky=tk.W)
+        self.add_phone_entry = tk.Entry(self, width=15, bg="white").grid(row=3, column=3)
+
+        # Email
+        self.add_emp_id_label = tk.Label(self, text="Email:").grid(row=3, column=4, pady=5, sticky=tk.W)
+        self.add_emp_id_entry = tk.Entry(self, width=15, bg="white")
+        self.add_emp_id_entry.grid(row=3, column=5)
+
+        # Pay Method
+        self.add_payment_method_label = tk.Label(self, text="Pay Method:").grid(row=4, column=0, pady=5, sticky=tk.W)
+        self.add_payment_method_entry = tk.OptionMenu(self, self.pay_method, "Mail", "ACH")
+        self.add_payment_method_entry.grid(row=4, column=1)
+        self.add_payment_method_entry.config(width=15)
+
+        self.pay_method.trace("w", self.show_hide_bank)
+
+        # Routing Number
+        self.add_routing_number_label = tk.Label(self, text="Routing #:")
+        self.add_routing_number_label.grid(row=4, column=2, pady=5, sticky=tk.W)
+        self.add_routing_number_entry = tk.Entry(self, width=15, bg="white")
+        self.add_routing_number_entry.grid(row=4, column=3)
+
+        # Account Number
+        self.add_account_number_label = tk.Label(self, text="Account #:")
+        self.add_account_number_label.grid(row=4, column=4, pady=5, sticky=tk.W)
+        self.add_account_number_entry = tk.Entry(self, width=15, bg="white")
+        self.add_account_number_entry.grid(row=4, column=5)
+
+        # Social Security
+        self.add_ss_label = tk.Label(self, text="Social Security:")
+        self.add_ss_label.grid(row=5, column=0, pady=5, sticky=tk.W)
+        self.add_ss_entry = tk.Entry(self, width=15, bg="white")
+        self.add_ss_entry.grid(row=5, column=1)
+
+        # Birthday
+        self.add_dob_label = tk.Label(self, text="DOB:")
+        self.add_dob_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+        self.add_dob_entry = tk.Entry(self, width=15, bg="white")
+        self.add_dob_entry.grid(row=5, column=3)
+
+        # Start Date
+        self.add_start_date_label = tk.Label(self, text="Start Date:")
+        self.add_start_date_label.grid(row=6, column=0, pady=5, sticky=tk.W)
+        self.add_start_date_entry = tk.Entry(self, width=15, bg="white")
+        self.add_start_date_entry.grid(row=6, column=1)
+
+        # End Date
+        self.add_end_date_label = tk.Label(self, text="End Date:")
+        self.add_end_date_label.grid(row=6, column=2, pady=5, sticky=tk.W)
+        self.add_end_date_entry = tk.Entry(self, width=15, bg="white")
+        self.add_end_date_entry.grid(row=6, column=3)
+
+        # Permission
+        self.add_permission_label = tk.Label(self, text="Permission:")
+        self.add_permission_label.grid(row=7, column=0, pady=5, sticky=tk.W)
+        self.add_permission_entry = tk.Entry(self, width=15, bg="white")
+        self.add_permission_entry.grid(row=7, column=1)
+
+        # Title
+        self.add_title_label = tk.Label(self, text="Title:")
+        self.add_title_label.grid(row=7, column=2, pady=5, sticky=tk.W)
+        self.add_title_entry = tk.Entry(self, width=15, bg="white")
+        self.add_title_entry.grid(row=7, column=3)
+
+        # Department
+        self.add_dept_label = tk.Label(self, text="Dept:")
+        self.add_dept_label.grid(row=7, column=4, pady=5, sticky=tk.W)
+        self.add_dept_entry = tk.Entry(self, width=15, bg="white")
+        self.add_dept_entry.grid(row=7, column=5)
+
+        # Buttons
+        self.submit_button = tk.Button(self, text="Submit", command=self.save_info)
+        self.submit_button.grid(row=11, column=1, pady=15)
+        self.cancel_button = tk.Button(self, text="Cancel", command=self.save_info)
+        self.cancel_button.grid(row=11, column=0)
 
     def save_info(self):
         """Save the new Employee in the database using the database functions"""
         msg.showinfo(title="Saved", message="Employee data will be saved")
-        #todo
+        # todo
 
-        #Save data to database
-        #(e = )self.controller.database.create_employee(<where does id come from?>)
+        # Save data to database
+        # (e = )self.controller.database.create_employee(<where does id come from?>)
         # Set fields on e
-        #self.controller.database.set_employee_data()
+        # self.controller.database.set_employee_data()
 
-        #Return to previous frame
+        # Return to previous frame
         self.controller.present_frame("EmployeeList")
 
     def check_permission(self, id):
@@ -50,3 +143,16 @@ class AddEmployee(tk.Frame):
         returns true if logged in as administrator
         returns false otherwise"""
         return False
+
+    def show_hide_bank(self, *args):
+        """Shows or hide the values for Direct Deposit based on input"""
+        if self.pay_method.get() == "Mail":
+            self.add_routing_number_label.grid_forget()
+            self.add_routing_number_entry.grid_forget()
+            self.add_account_number_label.grid_forget()
+            self.add_account_number_entry.grid_forget()
+        if self.pay_method.get() == "ACH":
+            self.add_routing_number_label.grid(row=4, column=2, pady=5, sticky=tk.W)
+            self.add_routing_number_entry.grid(row=4, column=3)
+            self.add_account_number_label.grid(row=4, column=4, pady=5, sticky=tk.W)
+            self.add_account_number_entry.grid(row=4, column=5)
