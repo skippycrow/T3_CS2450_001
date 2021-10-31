31ed684f4ae7060577ac11e227b059f1e8ab35a9
skippycrow <70984355+skippycrow@users.noreply.github.com>
2021-10-31 09:45:27 -0600

    Add Employee Database logic added

--- project/Add_Employee_Frame.py
+++ project/Add_Employee_Frame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox as msg
+from tkinter import ttk
 
 
 class AddEmployee(tk.Frame):
@@ -8,6 +9,8 @@ class AddEmployee(tk.Frame):
 
         self.controller = controller
         self.pay_method = tk.StringVar(self)
+        self.pay_type = tk.StringVar(self)
+        self.cur_perm = tk.StringVar(self)
 
         self.title_label = tk.Label(self, text="Add Employee")
         self.title_label.grid(row=0, columnspan=6, pady=10, sticky=tk.N)
@@ -53,9 +56,10 @@ class AddEmployee(tk.Frame):
         self.add_phone_entry = tk.Entry(self, width=15, bg="white").grid(row=3, column=3)
 
         # Email
-        self.add_emp_id_label = tk.Label(self, text="Email:").grid(row=3, column=4, pady=5, sticky=tk.W)
-        self.add_emp_id_entry = tk.Entry(self, width=15, bg="white")
-        self.add_emp_id_entry.grid(row=3, column=5)
+        self.add_email_label = tk.Label(self, text="Email:")
+        self.add_email_label.grid(row=3, column=4, pady=5, sticky=tk.W)
+        self.add_email_entry = tk.Entry(self, width=15, bg="white")
+        self.add_email_entry.grid(row=3, column=5)
 
         # Pay Method
         self.add_payment_method_label = tk.Label(self, text="Pay Method:").grid(row=4, column=0, pady=5, sticky=tk.W)
@@ -77,47 +81,80 @@ class AddEmployee(tk.Frame):
         self.add_account_number_entry = tk.Entry(self, width=15, bg="white")
         self.add_account_number_entry.grid(row=4, column=5)
 
+        self.types = ("Salary", "Hourly", "Commission")
+
+        # Classification
+        self.add_class_label = tk.Label(self, text="Pay Type:")
+        self.add_class_label.grid(row=5, column=0, pady=5, sticky=tk.W)
+        self.add_class_entry = tk.OptionMenu(self, self.pay_type, *self.types, command=self.show_hide_type)
+        self.add_class_entry.grid(row=5, column=1)
+        self.add_class_entry.config(width=15)
+
+        # Salary Amount
+        self.add_salary_label = tk.Label(self, text="Salary Amount:")
+        self.add_salary_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+        self.add_salary_entry = tk.Entry(self, width=15, bg="white")
+        self.add_salary_entry.grid(row=5, column=3)
+        self.add_salary_label.grid_forget()
+        self.add_salary_entry.grid_forget()
+
+        # Hourly rate
+        self.add_hourly_label = tk.Label(self, text="Rate:")
+        self.add_hourly_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+        self.add_hourly_entry = tk.Entry(self, width=15, bg="white")
+        self.add_hourly_entry.grid(row=5, column=3)
+        self.add_hourly_entry.grid_forget()
+        self.add_hourly_label.grid_forget()
+
+        # Commission rate
+        self.add_comm_label = tk.Label(self, text="Rate:")
+        self.add_comm_label.grid(row=5, column=4, pady=5, sticky=tk.W)
+        self.add_comm_entry = tk.Entry(self, width=15, bg="white")
+        self.add_comm_entry.grid(row=5, column=5)
+        self.add_comm_entry.grid_forget()
+        self.add_comm_label.grid_forget()
+
         # Social Security
         self.add_ss_label = tk.Label(self, text="Social Security:")
-        self.add_ss_label.grid(row=5, column=0, pady=5, sticky=tk.W)
+        self.add_ss_label.grid(row=6, column=0, pady=5, sticky=tk.W)
         self.add_ss_entry = tk.Entry(self, width=15, bg="white")
-        self.add_ss_entry.grid(row=5, column=1)
+        self.add_ss_entry.grid(row=6, column=1)
 
         # Birthday
         self.add_dob_label = tk.Label(self, text="DOB:")
-        self.add_dob_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+        self.add_dob_label.grid(row=6, column=2, pady=5, sticky=tk.W)
         self.add_dob_entry = tk.Entry(self, width=15, bg="white")
-        self.add_dob_entry.grid(row=5, column=3)
+        self.add_dob_entry.grid(row=6, column=3)
 
         # Start Date
         self.add_start_date_label = tk.Label(self, text="Start Date:")
-        self.add_start_date_label.grid(row=6, column=0, pady=5, sticky=tk.W)
+        self.add_start_date_label.grid(row=7, column=0, pady=5, sticky=tk.W)
         self.add_start_date_entry = tk.Entry(self, width=15, bg="white")
-        self.add_start_date_entry.grid(row=6, column=1)
+        self.add_start_date_entry.grid(row=7, column=1)
 
         # End Date
         self.add_end_date_label = tk.Label(self, text="End Date:")
-        self.add_end_date_label.grid(row=6, column=2, pady=5, sticky=tk.W)
+        self.add_end_date_label.grid(row=7, column=2, pady=5, sticky=tk.W)
         self.add_end_date_entry = tk.Entry(self, width=15, bg="white")
-        self.add_end_date_entry.grid(row=6, column=3)
+        self.add_end_date_entry.grid(row=7, column=3)
 
         # Permission
         self.add_permission_label = tk.Label(self, text="Permission:")
-        self.add_permission_label.grid(row=7, column=0, pady=5, sticky=tk.W)
-        self.add_permission_entry = tk.Entry(self, width=15, bg="white")
-        self.add_permission_entry.grid(row=7, column=1)
+        self.add_permission_label.grid(row=8, column=0, pady=5, sticky=tk.W)
+        self.add_permission_entry = tk.OptionMenu(self, self.cur_perm, "Admin", "Employee")
+        self.add_permission_entry.grid(row=8, column=1)
 
         # Title
         self.add_title_label = tk.Label(self, text="Title:")
-        self.add_title_label.grid(row=7, column=2, pady=5, sticky=tk.W)
+        self.add_title_label.grid(row=8, column=2, pady=5, sticky=tk.W)
         self.add_title_entry = tk.Entry(self, width=15, bg="white")
-        self.add_title_entry.grid(row=7, column=3)
+        self.add_title_entry.grid(row=8, column=3)
 
         # Department
         self.add_dept_label = tk.Label(self, text="Dept:")
-        self.add_dept_label.grid(row=7, column=4, pady=5, sticky=tk.W)
+        self.add_dept_label.grid(row=8, column=4, pady=5, sticky=tk.W)
         self.add_dept_entry = tk.Entry(self, width=15, bg="white")
-        self.add_dept_entry.grid(row=7, column=5)
+        self.add_dept_entry.grid(row=8, column=5)
 
         # Buttons
         self.submit_button = tk.Button(self, text="Submit", command=self.save_info)
@@ -127,13 +164,53 @@ class AddEmployee(tk.Frame):
 
     def save_info(self):
         """Save the new Employee in the database using the database functions"""
-        msg.showinfo(title="Saved", message="Employee data will be saved")
-        # todo
 
+        classification = 0
+        if self.pay_type.get() == "Salary":
+            classification = 1
+        if self.pay_type.get() == "Hourly":
+            classification = 2
+        if self.pay_type.get() == "Commission":
+            classification = 3
+
+        method = 0
+        if self.pay_method.get() == "ACH":
+            method = 1
+        if self.pay_method.get() == "Mail":
+            method = 2
+
+        # Done
         # Save data to database
         # (e = )self.controller.database.create_employee(<where does id come from?>)
         # Set fields on e
         # self.controller.database.set_employee_data()
+        e_id = self.controller.database.create_employee(self.add_emp_id_entry.get())
+
+        self.controller.database.set_employee_data(e_id, 'name_first', self.add_first_name_entry.get())
+        self.controller.database.set_employee_data(e_id, 'name_last', self.add_last_name_entry.get())
+        self.controller.database.set_employee_data(e_id, 'contact_phone', self.add_phone_entry.get())
+        self.controller.database.set_employee_data(e_id, 'contact_email', "email")
+        self.controller.database.set_employee_data(e_id, 'address_street', self.add_street_entry.get())
+        self.controller.database.set_employee_data(e_id, 'address_city', self.add_city_entry.get())
+        self.controller.database.set_employee_data(e_id, 'address_state', self.add_state_entry.get())
+        self.controller.database.set_employee_data(e_id, 'zip_code', self.add_zip_entry.get())
+        self.controller.database.set_employee_data(e_id, 'classification', classification)
+        self.controller.database.set_employee_data(e_id, 'pay_method', method)
+        self.controller.database.set_employee_data(e_id, 'salary', self.add_salary_entry.get())
+        self.controller.database.set_employee_data(e_id, 'hourly', self.add_hourly_entry.get())
+        self.controller.database.set_employee_data(e_id, 'commission_rate', self.add_comm_entry.get())
+        self.controller.database.set_employee_data(e_id, 'routing_number', self.add_routing_number_entry.get())
+        self.controller.database.set_employee_data(e_id, 'Account', self.add_account_number_entry.get())
+        self.controller.database.set_employee_data(e_id, 'social_security', self.add_ss_entry.get())
+        self.controller.database.set_employee_data(e_id, 'birthday', self.add_dob_entry.get())
+        self.controller.database.set_employee_data(e_id, 'start_date', self.add_start_date_entry.get())
+        self.controller.database.set_employee_data(e_id, 'permission', self.cur_perm)
+        self.controller.database.set_employee_data(e_id, 'title', self.add_title_entry.get())
+        self.controller.database.set_employee_data(e_id, 'dept', self.add_dept_entry.get())
+        self.controller.database.set_employee_data(e_id, 'email', self.add_email_entry.get())
+        self.controller.database.set_employee_data(e_id, 'end_date', self.add_end_date_entry.get())
+
+        msg.showinfo(title="Saved", message="Employee Added")
 
         # Return to previous frame
         self.controller.present_frame("EmployeeList")
@@ -156,3 +233,27 @@ class AddEmployee(tk.Frame):
             self.add_routing_number_entry.grid(row=4, column=3)
             self.add_account_number_label.grid(row=4, column=4, pady=5, sticky=tk.W)
             self.add_account_number_entry.grid(row=4, column=5)
+
+    def show_hide_type(self, *args):
+        """Shows or hide salary/hourly/commission based on input"""
+        if self.pay_type.get() == "Salary":
+            self.add_salary_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+            self.add_salary_entry.grid(row=5, column=3)
+            self.add_hourly_entry.grid_forget()
+            self.add_hourly_label.grid_forget()
+            self.add_comm_entry.grid_forget()
+            self.add_comm_label.grid_forget()
+        if self.pay_type.get() == "Hourly":
+            self.add_salary_label.grid_forget()
+            self.add_salary_entry.grid_forget()
+            self.add_hourly_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+            self.add_hourly_entry.grid(row=5, column=3)
+            self.add_comm_entry.grid_forget()
+            self.add_comm_label.grid_forget()
+        if self.pay_type.get() == "Commission":
+            self.add_salary_label.grid(row=5, column=2, pady=5, sticky=tk.W)
+            self.add_salary_entry.grid(row=5, column=3)
+            self.add_hourly_entry.grid_forget()
+            self.add_hourly_label.grid_forget()
+            self.add_comm_label.grid(row=5, column=4, pady=5, sticky=tk.W)
+            self.add_comm_entry.grid(row=5, column=5)
