dc4adc65922ddee73399f92bd609f05d7498fd41
ckang21 <81390797+ckang21@users.noreply.github.com>
2021-10-31 21:39:48 -0600

    Update Login_Frame.py

--- project/Login_Frame.py
+++ project/Login_Frame.py
@@ -4,8 +4,7 @@ from tkinter import messagebox as msg
 import auth
 
 #auth.read_auth_data(path)
-# TODO: decide where password data csv will be stored
-# most likely in the same project directory
+
 
 class LoginFrame(tk.Frame):
     def __init__(self, parent, controller):
@@ -17,10 +16,10 @@ class LoginFrame(tk.Frame):
 
         # Initialize controller
         self.controller = controller
-
         id_label = tk.Label(self, text = "Employee ID").grid(row=3, column = 3)
         self.e_id = tk.StringVar()
         e_id_input = tk.Entry(self, textvariable = self.e_id).grid(row = 3, column = 4)
+        self.controller.add_data("LoginFrame_id", self.e_id)
         password_label = tk.Label(self, text = "Password").grid(row = 3, column = 7)
         self.password = tk.StringVar()
         password_input = tk.Entry(self, textvariable = self.password, show = '*').grid(row = 3, column = 8)
@@ -28,18 +27,17 @@ class LoginFrame(tk.Frame):
         # Add login button, calls checkLogin
         login_button = tk.Button(self, text = "Login", command = self.check_login).grid(row = 8, column = 5)
 
-    # Inputs will be passed in here to verify validity
-    # Will finish implementation when username and password csv are completed
+    
     def check_login(self):
-        # placeholder for veryifying the user and password
-        #results = auth.verify_password(self.e_id, self.password)
-        #if results == auth.PASSWORD_CORRECT:
-        self.controller.present_frame("LandingFrame")
-        #loginPage.removeLogin()
-        #else:
-         
-        #self.failLogin()
-        return
+        """
+        results = auth.verify_password(self.e_id, self.password)
+        if results == auth.PASSWORD_CORRECT:
+            self.controller.present_frame("LandingFrame", ssef.e_id)
+            loginPage.removeLogin()
+        else:
+         self.failLogin()
+         """
+        pass
         
     # Will call up a warning box if the credentials are invalid
     def fail_login(self):
