535b5fd0e491d059f63dd572dea6b2797728ee4e
Zach <10695868@uvu.edu>
2021-11-01 12:31:51 -0600

    improve script's pylint score

--- project/database.py
+++ project/database.py
@@ -1,4 +1,6 @@
+"""Defines EmployeeDatabase and Employee classes"""
 import csv
+
 
 # needs some way to differentiate what fields are accessible by each access level
 # might be here, might be elsewhere
@@ -29,8 +31,8 @@ DATA_FIELDS = [
 ]
 
 class EmployeeDatabase:
-    # Employee Database class
-    # Holds id and info for each employee
+    """Employee Database class
+    Holds id and info for each employee"""
 
     def __init__(self):
         # Dictionary of employees
@@ -38,77 +40,93 @@ class EmployeeDatabase:
         self.employees = {}
 
     def save_to_file(self, path):
+        """saves data to csv file at path"""
         with open(path, mode='w') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames = ['id'] + DATA_FIELDS)
             writer.writeheader()
-            for e_id, e in self.employees.items():
+            for e_id, employee in self.employees.items():
                 row = {}
                 row['id'] = e_id
                 for field in DATA_FIELDS:
-                    row[field] = getattr(e, field)
+                    row[field] = getattr(employee, field)
                 writer.writerow(row)
 
     def load_from_file(self, path):
+        """reads data from csv file at path"""
         with open(path, mode='r') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
-                e = self.create_employee(row['ID'])
+                new_employee = self.create_employee(row['ID'])
                 for field in DATA_FIELDS:
                     if field in row:
-                        setattr(e, field, row[field])
+                        setattr(new_employee, field, row[field])
 
     def create_employee(self, e_id, override_existing = False):
+        """creates employee object, assigning the id
+        will not by default create one if the id exists already
+        created employee object is then returned"""
+
         if not isinstance(e_id, str):
             raise TypeError('ID should be a String')
 
         if e_id in self.employees and not override_existing:
-            raise ValueError('Specified id is already present in database\nIf this is intended, pass in True for override_existing')
-        else:
-            self.employees[e_id] = Employee(e_id)
-            return self.employees[e_id]
-    
+            raise ValueError('''Specified id is already present in database
+                If this is intended, pass in True for override_existing''')
+
+        self.employees[e_id] = Employee(e_id)
+        return self.employees[e_id]
+
     def get_employee(self, e_id):
+        """returns employee with given id"""
         if e_id in self.employees:
             return self.employees[e_id]
-        else:
-            raise IndexError('id not found in employee database')
-    
+
+        raise IndexError('id not found in employee database')
+
     def erase_employee(self, e_id):
+        """removes employee fram database"""
         if e_id in self.employees:
             self.employees.pop(e_id)
-    
+
     def set_employee_data(self, e_id, field, value):
+        """sets field property of employrr with given id"""
+
         if not field in DATA_FIELDS:
             raise AttributeError('specified field not found in DATA_FIELDS list')
-        elif not e_id in self.employees:
+
+        if not e_id in self.employees:
             raise IndexError('id not found in employee database')
-        else:
-            setattr(self.employees[e_id], field, value)
-    
+
+        setattr(self.employees[e_id], field, value)
+
     def get_employee_data(self, e_id, field):
+        """returns the field property of employee with given id"""
+
         if not field in DATA_FIELDS:
             raise AttributeError('specified field not found in DATA_FIELDS list')
-        elif not e_id in self.employees:
+
+        if not e_id in self.employees:
             raise IndexError('id not found in employee database')
-        else:
-            return getattr(self.employees[e_id], field)
-    
+
+        return getattr(self.employees[e_id], field)
+
     # TODO: implement searching by name
 
 class Employee:
-    # class for single Employee data
-    # should primarily be manmaged via containing EmployeeDatabase
+    """class for single Employee data
+    should primarily be manmaged via containing EmployeeDatabase"""
 
     def __init__(self, e_id):
         self.id = e_id # do **NOT** set this in any other place
+        self.name_first = ''
+        self.name_last = ''
         for field in DATA_FIELDS:
             setattr(self, field, '')
 
     def get_full_name(self):
+        """returns full name in last, first format"""
         return self.name_last + ', ' + self.name_first
-    
-    def get_id(self):
-        return self.id
 
-    def set_id(self, new_id):
-        raise ValueError('Don\'t')
+    def get_id(self):
+        """returns the id of this employee"""
+        return self.id
