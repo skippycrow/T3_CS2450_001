927519e14cf82a14de4505d610691c456a21c043
Jon <jonathan.morgan.computer@gmail.com>
2021-10-31 22:43:36 -0600

    Pushing tylers new employee profile frame
    Uploading Tyler's new profile frame

--- project/Employee_Profile_Frame.py
+++ project/Employee_Profile_Frame.py
@@ -8,19 +8,51 @@ class EmployeeProfile(tk.Frame):
 
         #Initialize controller
         self.controller = controller
+
+    
+
+        self.e_id = "688997"        #Will need to get id# from login or employee list
+        self.name = self.controller.database.get_employee_data(self.e_id, "name_first") + " " + self.controller.database.get_employee_data(self.e_id, "name_last")
+        self.title = "Employee"     #self.controller.database.get_employee_data(self.e_id, "title")
+        self.dept = "Faculty"       #self.controller.database.get_employee_data(self.e_id, "dept")
+        self.phone = self.controller.database.get_employee_data(self.e_id, "contact_phone")
+        self.email = self.controller.database.get_employee_data(self.e_id, "contact_email")
+        #Syntax Error:
+        #self.address = self.controller.database.get_employee_data(self.e_id, "address_street") + " " self.controller.database.get_employee_data(self.e_id, "address_city") + ", " + self.controller.database.get_employee_data(self.e_id, "address_state") + " " + self.controller.database.get_employee_data(self.e_id, "zip_code")
+        self.classification = self.controller.database.get_employee_data(self.e_id, "classification")
+        self.pay_method = self.controller.database.get_employee_data(self.e_id, "pay_method")
+        self.salary = self.controller.database.get_employee_data(self.e_id, "salary")
+        self.hourly = self.controller.database.get_employee_data(self.e_id, "hourly")
+        self.commission = self.controller.database.get_employee_data(self.e_id, "commission_rate")
+        self.routing = self.controller.database.get_employee_data(self.e_id, "routing_number")
+        self.account = self.controller.database.get_employee_data(self.e_id, "Account") #database.py has this as the only capitalized field
+        self.ssn = self.controller.database.get_employee_data(self.e_id, "social_security")
+        self.birthday = self.controller.database.get_employee_data(self.e_id, "birthday")
+        self.start_date = self.controller.database.get_employee_data(self.e_id, "start_date")
+        self.permission = self.controller.database.get_employee_data(self.e_id, "permission")
+        self.end_date = self.controller.database.get_employee_data(self.e_id, "end_date")
        
-        tk.Label(self, text="[Employee Name]",justify='center').grid(column=0, row=0, columnspan=3)
-        tk.Label(self, text="[Employee ID number]",justify='center').grid(column=0, row=1, columnspan=3)
-        tk.Label(self, text="[position] -- [department]").grid(row=2, column=0, sticky=tk.W)
-        tk.Label(self, text="Office Phone: (555) 555-5555").grid(column=0, row=3, columnspan=1, sticky=tk.W)
-        tk.Label(self, text="Email: sample@uvu.edu").grid(row=4, column=0, columnspan=1, sticky=tk.W)
+        tk.Label(self, text=self.name, font = "none 18 bold").grid(column=0, row=0, sticky = tk.W)
+        tk.Label(self, text=self.e_id, font = "none 12 bold").grid(column=0, row=1, sticky = tk.W)
+        tk.Label(self, text="Position: " + self.title, font = "none 12 bold").grid(column=0, row=2, sticky = tk.W)
+        tk.Label(self, text="Department: " + self.dept, font = "none 12 bold").grid(row=2, column=2, sticky=tk.W)
+        tk.Label(self, text="Office Phone: " + self.phone).grid(column=0, row=3, columnspan=1, sticky=tk.W)
+        tk.Label(self, text="Email: " + self.email).grid(row=3, column=2, columnspan=1, sticky=tk.W)
         tk.Label(self, text=' ').grid(row=5,column=0)
 
         if self.check_permission(23): #the logged-in employee's number will be passed in
-            tk.Label(self, text='<<Other employee info>>').grid(row=6, column=0, sticky=tk.W)
-            tk.Button(self, text="Edit Employee", command=self.stand_in).grid(column=1, row=10)
+            tk.Label(self, text='<<Other employee info will display here>>').grid(row=6, column=0, sticky=tk.W)
+            tk.Label(self, text="Address: ").grid(row=7, column=0, sticky=tk.W)    #FIX address line then add in variable
+            tk.Label(self, text="Employee Classification: " + self.classification).grid(row=8, column=0, sticky=tk.W)
+            tk.Label(self, text="Pay Method: " + self.pay_method).grid(row=9, column=0, sticky=tk.W)
+            tk.Label(self, text="Salary: " + self.salary).grid(row=9, column=1, sticky=tk.W)
+            tk.Label(self, text="Hourly: " + self.hourly).grid(row=9, column=2, sticky=tk.W)
+            tk.Label(self, text="Commission: " + self.commission).grid(row=9, column=3, sticky=tk.W)
+            tk.Label(self, text="<<routing #, account #, and other data fields to be implemented").grid(row=10, column=0, sticky=tk.W)
 
-        tk.Button(self, text="Back", command = lambda: controller.present_frame("LandingFrame")).grid(column=2, row=10)
+            tk.Button(self, text="Edit Employee", command=self.stand_in).grid(column=1, row=20)
+
+        tk.Button(self, text="Back", command = lambda: controller.present_frame("LandingFrame")).grid(column=2, row=20)
     
     def stand_in():
         pass
@@ -29,4 +61,4 @@ class EmployeeProfile(tk.Frame):
         """returns true if logged in employee matches profile id#
         returns true if logged in as administrator
         returns false otherwise"""
-        return False
+        return True
