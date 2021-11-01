495c12358f4540882c5139711718c7e6954f703d
ckang21 <81390797+ckang21@users.noreply.github.com>
2021-10-31 21:40:06 -0600

    Update Landing_Frame.py

--- project/Landing_Frame.py
+++ project/Landing_Frame.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox as msg
 
+#Need to import Employee Profile Frame
+#through controller add the id to the data and have it populate the profile page.
+
 class LandingFrame(tk.Frame):
     #Constructor class
     def __init__(self, parent, controller):
@@ -9,9 +12,8 @@ class LandingFrame(tk.Frame):
 
         #Initialize controller
         self.controller = controller
-
-        self.user_id = None
         self.login_command = None
+        self.user_id = self.controller.app_data["LoginPage_id"]
         
         #Set the format of the buttons and all of their locations
         logout_button = tk.Button(self, text = "Logout", command = lambda: controller.present_frame("LoginFrame"))
@@ -28,15 +30,21 @@ class LandingFrame(tk.Frame):
         self.login_command = command
     
     #Once implemented this command will take the user to his personal profile
-    #Currently unavailable
     def on_profile_clicked(self):
-        return
-    
-    #Once implemented this command will take a user with higher permission to a list of Employees page
-    #Currently unavailable
+        #
+        if EmployeeProfile.check_permission(id) == true:
+            self.controller.present_frame("EmployeeProfile")
+
+    #This command will take a user with higher permission to a list of Employees page
     def on_employee_clicked(self):
-        return
+        #Permission needs to be fully implemented for this to check admin level.
+        #if employee.get_employee_data(self.id, permission) == true:
+        self.controller.present_frame("EmployeeList")
+        
+        #else:
+        #    msg.showwarning(title="Insufficient Permission", message="Incorrect Permission Level")
     
     #This command will return the user to the login page and remove the current frame
     def on_logout_clicked(self):
         self.login_command()
+        #close LandingPage
