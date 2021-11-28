91e00104f41d7a267213ddcaa7a69e22773b5f4a
ACast1 <91992975+ACast1@users.noreply.github.com>
2021-11-27 20:44:39 -0700

    Cleaned up code

--- project/Login_Frame.py
+++ project/Login_Frame.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox as msg
-
 import auth
 
+#Load passwords
 auth.read_auth_data('Resources/passwords.csv')
-
 
 class LoginFrame(tk.Frame):
     def __init__(self, parent, controller):
@@ -16,41 +15,45 @@ class LoginFrame(tk.Frame):
 
         # Initialize controller
         self.controller = controller
-        
 
-        # Style frame
-        id_label = tk.Label(self, text = "Employee ID", font = "none 20 bold").grid(row= 1, column = 1)
+        #Style frame
+        id_label = tk.Label(self, text = "Employee ID", font = "none 20 bold").grid(row = 1, column = 1)
         self.e_id = tk.StringVar()
         e_id_input = tk.Entry(self, textvariable = self.e_id, width = 70).grid(row = 1, column = 2)
         self.controller.add_data("user_id", self.e_id)
         password_label = tk.Label(self, text = "Password", font = "none 20 bold").grid(row = 2, column = 1)
         self.password = tk.StringVar()
         password_input = tk.Entry(self, textvariable = self.password, show = '*', width = 70).grid(row = 2, column = 2)
-        
-        # Add login button, calls checkLogin
-        emptyrow3 = tk.Label(self, text = "").grid(row=3)
-        login_button = tk.Button(self, text = "Login", width = 20, height = 1, command = self.check_login).grid(row = 3, column = 2)
 
         #Set weight to surrounding row/col to center buttons on frame
         self.grid_rowconfigure(0, weight = 1)
         self.grid_rowconfigure(4, weight = 1)
         self.grid_columnconfigure(0, weight = 1)
         self.grid_columnconfigure(4, weight = 1)
+        
+        #Add login button, calls checkLogin
+        emptyrow3 = tk.Label(self, text = "").grid(row = 3)
+        login_button = tk.Button(self, text = "Login", width = 20, height = 1, command = self.check_login).grid(row = 3, column = 2)
 
     def update(self):
         pass
-
     
     def check_login(self):
+        #Store authentication
         results = auth.verify_password(self.e_id.get(), self.password.get())
         
+        #If authentication correct
         if results == auth.PASSWORD_CORRECT:
+            #Set user id in app data
             self.controller.app_data["LoginFrame_userID"] = self.e_id.get()
-            self.controller.present_frame("LandingFrame")
 
+            #Proceed to landing frame
+            self.controller.present_frame("LandingFrame")
+        
+        #Authentication incorrect
         else:
          self.fail_login()
         
-    # Will call up a warning box if the credentials are invalid
     def fail_login(self):
-         msg.showwarning(title="Failed Login", message="Incorrect Username or Password")
+         #Will call up a warning box if the credentials are invalid
+         msg.showwarning(title = "Failed Login", message = "Incorrect Username or Password")
