e3853ed0c627eda82a0dcb132dc49060d9aea0a1
ACast1 <91992975+ACast1@users.noreply.github.com>
2021-11-29 01:21:41 -0700

    Set user permission upon login in app_data

--- project/Login_Frame.py
+++ project/Login_Frame.py
@@ -7,33 +7,29 @@ auth.read_auth_data('Resources/passwords.csv')
 
 class LoginFrame(tk.Frame):
     def __init__(self, parent, controller):
-        # ID and password text boxes plus labels, save inputs to variables for future use
-        # Set up the frame and create all buttons
-
-        # Initialize tk
+        #Initialize tk
         tk.Frame.__init__(self, parent)
 
-        # Initialize controller
+        #Initialize controller
         self.controller = controller
 
-        #Style frame
-        id_label = tk.Label(self, text = "Employee ID", font = "none 20 bold").grid(row = 1, column = 1)
+        #Create input variables
         self.e_id = tk.StringVar()
-        e_id_input = tk.Entry(self, textvariable = self.e_id, width = 70).grid(row = 1, column = 2)
-        self.controller.add_data("user_id", self.e_id)
-        password_label = tk.Label(self, text = "Password", font = "none 20 bold").grid(row = 2, column = 1)
         self.password = tk.StringVar()
-        password_input = tk.Entry(self, textvariable = self.password, show = '*', width = 70).grid(row = 2, column = 2)
+
+        #Style frame
+        tk.Label(self, text = "Employee ID", font = "none 20 bold").grid(row = 1, column = 1)
+        self.e_id_input = tk.Entry(self, textvariable = self.e_id, width = 70).grid(row = 1, column = 2)
+        tk.Label(self, text = "Password", font = "none 20 bold").grid(row = 2, column = 1)
+        self.password_input = tk.Entry(self, textvariable = self.password, show = '*', width = 70).grid(row = 2, column = 2)
+        emptyrow3 = tk.Label(self, text = "").grid(row = 3)
+        self.login_button = tk.Button(self, text = "Login", width = 20, height = 1, command = self.check_login).grid(row = 3, column = 2)
 
         #Set weight to surrounding row/col to center buttons on frame
         self.grid_rowconfigure(0, weight = 1)
         self.grid_rowconfigure(4, weight = 1)
         self.grid_columnconfigure(0, weight = 1)
         self.grid_columnconfigure(4, weight = 1)
-        
-        #Add login button, calls checkLogin
-        emptyrow3 = tk.Label(self, text = "").grid(row = 3)
-        login_button = tk.Button(self, text = "Login", width = 20, height = 1, command = self.check_login).grid(row = 3, column = 2)
 
     def update(self):
         pass
@@ -46,6 +42,8 @@ class LoginFrame(tk.Frame):
         if results == auth.PASSWORD_CORRECT:
             #Set user id in app data
             self.controller.app_data["LoginFrame_userID"] = self.e_id.get()
+            #Set user permission in app data
+            self.controller.app_data["LoginFrame_permission"] = self.controller.database.get_employee_data(self.e_id.get(), "permission")
 
             #Proceed to landing frame
             self.controller.present_frame("LandingFrame")
