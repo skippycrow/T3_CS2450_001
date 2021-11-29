b4153cdf12dec4bfc0ebff33650c48d13121b0b6
ACast1 <91992975+ACast1@users.noreply.github.com>
2021-11-29 01:57:30 -0700

    Implemented permission functionality
    When the profile is accessed through the landing page, the profile will display all of the user's info to them. When the profile is accessed through the employee list frame, it will show the selected employee's info. If the user is an admin, they will see all of the selected employee's info. If the user is another employee, they will see only some of the selected employee's info

--- project/Employee_Profile_Frame.py
+++ project/Employee_Profile_Frame.py
@@ -15,39 +15,50 @@ class EmployeeProfile(tk.Frame):
             #Destroy the widget
             widget.destroy()
 
-        #Get data
-        self.e_id = self.controller.app_data["LoginFrame_userID"]    #Will need to get id# from login or employee list
-        self.name = self.controller.database.get_employee_data(self.e_id, "name_first") + " " + self.controller.database.get_employee_data(self.e_id, "name_last")
-        self.title = "Employee"     #self.controller.database.get_employee_data(self.e_id, "title")
-        self.dept = "Faculty"       #self.controller.database.get_employee_data(self.e_id, "dept")
-        self.phone = self.controller.database.get_employee_data(self.e_id, "contact_phone")
-        self.email = self.controller.database.get_employee_data(self.e_id, "contact_email")
+        #If accessed employee profile from EmployeeList frame with another employee selected
+        if self.controller.app_data["EmployeeListFrame_showSelectedEmployee"]:
+            #Set employee to present as the selected employee from the EmployeeList frame
+            self.e_id_to_present = self.controller.app_data["EmployeeListFrame_selectedEmployeeID"]
+
+        #Employee profile accessed regularly through the landing page
+        else:
+            #Set employee to present as the currently logged in user
+            self.e_id_to_present = self.controller.app_data["LoginFrame_userID"]
+
+        #Get employee to present data
+        self.name = self.controller.database.get_employee_data(self.e_id_to_present, "name_first") + " " + self.controller.database.get_employee_data(self.e_id_to_present, "name_last")
+        self.title = "Employee"     #self.controller.database.get_employee_data(self.e_id_to_present, "title")
+        self.dept = "Faculty"       #self.controller.database.get_employee_data(self.e_id_to_present, "dept")
+        self.phone = self.controller.database.get_employee_data(self.e_id_to_present, "contact_phone")
+        self.email = self.controller.database.get_employee_data(self.e_id_to_present, "contact_email")
         #Syntax Error:
-        #self.address = self.controller.database.get_employee_data(self.e_id, "address_street") + " " self.controller.database.get_employee_data(self.e_id, "address_city") + ", " + self.controller.database.get_employee_data(self.e_id, "address_state") + " " + self.controller.database.get_employee_data(self.e_id, "zip_code")
-        self.classification = self.controller.database.get_employee_data(self.e_id, "classification")
-        self.pay_method = self.controller.database.get_employee_data(self.e_id, "pay_method")
-        self.salary = self.controller.database.get_employee_data(self.e_id, "salary")
-        self.hourly = self.controller.database.get_employee_data(self.e_id, "hourly")
-        self.commission = self.controller.database.get_employee_data(self.e_id, "commission_rate")
-        self.routing = self.controller.database.get_employee_data(self.e_id, "routing_number")
-        self.account = self.controller.database.get_employee_data(self.e_id, "Account") #database.py has this as the only capitalized field
-        self.ssn = self.controller.database.get_employee_data(self.e_id, "social_security")
-        self.birthday = self.controller.database.get_employee_data(self.e_id, "birthday")
-        self.start_date = self.controller.database.get_employee_data(self.e_id, "start_date")
-        self.permission = self.controller.database.get_employee_data(self.e_id, "permission")
-        self.end_date = self.controller.database.get_employee_data(self.e_id, "end_date")
+        #self.address = self.controller.database.get_employee_data(self.e_id_to_present, "address_street") + " " self.controller.database.get_employee_data(self.e_id_to_present, "address_city") + ", " + self.controller.database.get_employee_data(self.e_id_to_present, "address_state") + " " + self.controller.database.get_employee_data(self.e_id_to_present, "zip_code")
+        self.classification = self.controller.database.get_employee_data(self.e_id_to_present, "classification")
+        self.pay_method = self.controller.database.get_employee_data(self.e_id_to_present, "pay_method")
+        self.salary = self.controller.database.get_employee_data(self.e_id_to_present, "salary")
+        self.hourly = self.controller.database.get_employee_data(self.e_id_to_present, "hourly")
+        self.commission = self.controller.database.get_employee_data(self.e_id_to_present, "commission_rate")
+        self.routing = self.controller.database.get_employee_data(self.e_id_to_present, "routing_number")
+        self.account = self.controller.database.get_employee_data(self.e_id_to_present, "account")
+        self.ssn = self.controller.database.get_employee_data(self.e_id_to_present, "social_security")
+        self.birthday = self.controller.database.get_employee_data(self.e_id_to_present, "birthday")
+        self.start_date = self.controller.database.get_employee_data(self.e_id_to_present, "start_date")
+        self.permission = self.controller.database.get_employee_data(self.e_id_to_present, "permission")
+        self.end_date = self.controller.database.get_employee_data(self.e_id_to_present, "end_date")
        
-        #Style page
+        #Style frame
         tk.Label(self, text = self.name, font = "none 18 bold").grid(column = 0, row = 0, sticky = tk.W)
-        tk.Label(self, text = self.e_id, font = "none 12 bold").grid(column = 0, row = 1, sticky = tk.W)
+        tk.Label(self, text = self.e_id_to_present, font = "none 12 bold").grid(column = 0, row = 1, sticky = tk.W)
         tk.Label(self, text = "Position: " + self.title, font = "none 12 bold").grid(column = 0, row = 2, sticky = tk.W)
         tk.Label(self, text = "Department: " + self.dept, font = "none 12 bold").grid(row = 2, column = 2, sticky = tk.W)
         tk.Label(self, text = "Office Phone: " + self.phone).grid(column = 0, row = 3, columnspan = 1, sticky = tk.W)
         tk.Label(self, text = "Email: " + self.email).grid(row = 3, column = 2, columnspan = 1, sticky = tk.W)
         tk.Label(self, text = ' ').grid(row = 5,column = 0)
-        tk.Button(self, text = "Back", command = lambda: self.controller.present_frame("LandingFrame")).grid(column = 2, row = 20)
+        tk.Button(self, text = "Back", command = self.clicked_back).grid(column = 2, row = 20)
 
-        if self.check_permission(23): #the logged-in employee's number will be passed in
+        #If permission level met
+        if self.show_full_view():
+            #Style frame with permission widgets
             tk.Label(self, text = '<<Other employee info will display here>>').grid(row = 6, column = 0, sticky = tk.W)
             tk.Label(self, text = "Address: ").grid(row = 7, column = 0, sticky = tk.W)    #FIX address line then add in variable
             tk.Label(self, text = "Employee Classification: " + self.classification).grid(row = 8, column = 0, sticky = tk.W)
@@ -56,13 +67,22 @@ class EmployeeProfile(tk.Frame):
             tk.Label(self, text = "Hourly: " + self.hourly).grid(row = 9, column = 2, sticky = tk.W)
             tk.Label(self, text = "Commission: " + self.commission).grid(row = 9, column = 3, sticky = tk.W)
             tk.Label(self, text = "<<routing #, account #, and other data fields to be implemented").grid(row = 10, column = 0, sticky = tk.W)
-            tk.Button(self, text = "Edit Employee", command=lambda: self.controller.present_frame("EditEmployee")).grid(column = 1, row = 20)
+            tk.Button(self, text = "Edit Employee", command = self.clicked_back).grid(column = 1, row = 20)
 
-    def stand_in(self):
-        pass
+    def show_full_view(self):
+        #If employee is viewing themselves
+        if self.controller.app_data["EmployeeListFrame_showSelectedEmployee"] is False:
+            return True
+        #If employee is an admin viewing another employees info
+        elif self.controller.app_data["LoginFrame_permission"] == "Admin":
+            return True
+        #Employee is viewing another employee without admin permission
+        else:
+            return False
 
-    def check_permission(self, id):
-        """returns true if logged in employee matches profile id#
-        returns true if logged in as administrator
-        returns false otherwise"""
-        return True
+    def clicked_back(self):
+        #Reset show selected employee flag
+        self.controller.app_data["EmployeeListFrame_showSelectedEmployee"] = False
+
+        #Proceed to landing frame
+        self.controller.present_frame("LandingFrame")
